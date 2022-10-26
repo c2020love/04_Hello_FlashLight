567f27116124fb6af6549095d09a0124bd3933f4
Connie <72365621+c2020love@users.noreply.github.com>
2022-10-26 18:58:53 +0800

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# 04_Hello_FlashLight
+    投光物:聚光灯FlashLight
---
+++ src/shader/cubefragement.glsl
@@ -0,0 +1,83 @@
+#version 330 core
+
+struct Material
+{
+	sampler2D specular;
+	sampler2D diffuse;
+	sampler2D raytexture;
+};
+
+struct Light
+{
+	vec3 position;	//聚光的位置向量（光源的位置）
+	vec3 direction;	//聚光的方向向量（光源的照射方向）
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+
+	float cutoff;	//切光角
+	float constant;
+	float linear;
+	float quadratic;
+};
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+out vec4 FragColor;  
+
+uniform Material material;
+uniform Light light;
+uniform vec3 viewPos;
+
+uniform float matrixlight;
+uniform float matrixmove;
+
+void main()
+{
+	//环境光
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse , TexCoords));
+
+	//漫反射
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = vec3(1.0f);
+	lightDir = normalize(light.position - FragPos);		//光的方向向量（当w为0的时候），而当w为1的时候就是光的位置向量
+	float diff = max(dot(norm,lightDir),0.0);
+	vec3 diffuse = light.diffuse * (diff * vec3(texture(material.diffuse , TexCoords)));
+
+	//镜面反射
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir,norm);
+	float spec = pow(max(dot(viewDir , reflectDir),0.0),64);
+	vec3 specular = light.specular * (spec * vec3(texture(material.specular , TexCoords)));
+
+	//点光源系数
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0/(light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+	diffuse *= attenuation;
+	specular *= attenuation;
+
+	//放射光
+	vec3 emission =vec3( matrixlight * texture(material.raytexture , vec2(TexCoords.x , TexCoords.y + matrixmove)));
+
+	//计算theta值，并将其与切光角cutoff对比，来判断这个片段是否在聚光的内部
+
+	float theta = dot(lightDir ,normalize(-light.direction));
+
+	vec3 result;
+	if(theta > light.cutoff)
+	{
+		result = (ambient + diffuse + specular) ;
+	}
+	else
+	{
+		result = light.ambient * vec3(texture(material.diffuse , TexCoords));
+	}
+
+	//最终结果
+
+
+	FragColor = vec4(result,1.0);
+}
+
